974dbd27fb7ad4bc465490cefe2c2bc3bb458854
Luis <luisrrf03@gmail.com>
2022-11-22 14:16:14 +0100

    Nuevos ejercicios

--- src/Ejercicios_de_repaso/Ejercicio7.java
+++ src/Ejercicios_de_repaso/Ejercicio7.java
@@ -13,17 +13,19 @@ public class Ejercicio7 {
 	
 		System.out.println("Introduzca el numero de horas que ha realizado el trabajador: ");
 		numHoras=teclado.nextInt();
-		salario=numHoras*12;
 		
 	
 		if (numHoras<= 0) {
 			System.out.println("El numero de horas introducido es incorrecto.");
 			
 		}else if (numHoras<=40) {
+			salario=numHoras*12;
 			System.out.println("El salario del trabajador en proporcion a las horas trabajads son "+
 					+salario+" euros");	
 			
+			
 		}else if (numHoras>40) {
+			salario=40*12;
 			diferencia=numHoras-40;
 			extras=diferencia *16;
 			salario=extras+salario;
